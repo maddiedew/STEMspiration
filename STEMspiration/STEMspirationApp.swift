5f04053bbda05496c2564f4533526b537baee7b5
Maddie Dew <maddiedew12@gmail.com>
2023-06-29 09:11:09 -0600

    Initial Commit

---
+++ STEMspiration/STEMspirationApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMspirationApp.swift
+//  STEMspiration
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct STEMspirationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
